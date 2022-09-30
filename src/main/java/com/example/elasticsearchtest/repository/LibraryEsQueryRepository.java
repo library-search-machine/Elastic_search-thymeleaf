f14ab8886f8fcdece8e39d7cae8e1cd40d40d4f4
DongDongShin <tlsehdgk87@naver.com>
2022-09-30 18:26:08 +0900

    feat: 검색문 추가 match_phrase term

--- src/main/java/com/example/elasticsearchtest/repository/LibraryEsQueryRepository.java
+++ src/main/java/com/example/elasticsearchtest/repository/LibraryEsQueryRepository.java
@@ -2,9 +2,13 @@ package com.example.elasticsearchtest.repository;
 
 import com.example.elasticsearchtest.domain.LibraryEs;
 import lombok.RequiredArgsConstructor;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.MatchPhraseQueryBuilder;
 import org.elasticsearch.index.query.MatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.sort.FieldSortBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -23,16 +27,17 @@ public class LibraryEsQueryRepository {
 
     private final ElasticsearchOperations operations;
     public Page<LibraryEs> findByBookName(Pageable pageable,String keyword) {
-        MatchPhraseQueryBuilder matchQueryBuilder = QueryBuilders.matchPhraseQuery("bookName",keyword);
-        NativeSearchQuery nativeSearchQuery = new NativeSearchQueryBuilder()
-                .withQuery(matchQueryBuilder)
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery()
+                .must(QueryBuilders.matchQuery("bookName",keyword))//문장이 완전 같지 않아도 검색
+                .should(QueryBuilders.termQuery("bookName.keyword",keyword))//완전히 일치하는 문자열
+                .should(QueryBuilders.matchPhraseQuery("bookName",keyword));//token값들을 가져오고 그 토큰들의 순서대로 검색해서 나온 검색값 return
+
+        NativeSearchQuery nativeSearchQuery= new NativeSearchQueryBuilder().withQuery(boolQueryBuilder)
                 .withPageable(pageable)
                 .build();
-        String query = nativeSearchQuery.getQuery().toString();
         SearchHits<LibraryEs> search = operations.search(nativeSearchQuery, LibraryEs.class);
         SearchPage<LibraryEs> searchHits = SearchHitSupport.searchPageFor(search, pageable);
         Page<LibraryEs> page = (Page)SearchHitSupport.unwrapSearchHits(searchHits);
-
         return  page;
     }
     public Page<LibraryEs> findByAuthors(Pageable pageable,String keyword) {
@@ -41,6 +46,8 @@ public class LibraryEsQueryRepository {
                 .withQuery(matchQueryBuilder)
                 .withPageable(pageable)
                 .build();
+
+
 
         String query = nativeSearchQuery.getQuery().toString();
         SearchHits<LibraryEs> search = operations.search(nativeSearchQuery, LibraryEs.class);
