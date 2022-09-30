2515810a4e79e7ddb92c558ac3d0f610493d22c8
scarlet0o0 <ghd7673@naver.com>
2022-09-30 16:29:04 +0900

    Fix: Buider와 쓸모없는거 제거

--- src/main/java/com/example/elasticsearchtest/dto/libraryRequestDto.java
+++ src/main/java/com/example/elasticsearchtest/dto/libraryRequestDto.java
@@ -1,19 +1,16 @@
 package com.example.elasticsearchtest.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class libraryRequestDto {
-    private String bookName;
 
+    private String bookName;
     private String authors;
-    private String classNum;
-    private String isbn13;
-    private String libraryName;
-    private String publicationYear;
     private String publisher;
-    private String vol;
 
 }
