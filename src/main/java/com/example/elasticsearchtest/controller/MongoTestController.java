257c47e7286d2fdedc097e1f6f49ed86f7a51a9d
DongDongShin <tlsehdgk87@naver.com>
2022-10-13 03:09:43 +0900

    feat: 수정 삭제하는 기능 추가

--- src/main/java/com/example/elasticsearchtest/controller/MongoTestController.java
+++ src/main/java/com/example/elasticsearchtest/controller/MongoTestController.java
@@ -2,39 +2,57 @@ package com.example.elasticsearchtest.controller;
 
 
 import com.example.elasticsearchtest.domain.BooksReview;
+import com.example.elasticsearchtest.repository.MongodbRepository;
 import com.example.elasticsearchtest.request.BookReviewRequest;
+
+import com.example.elasticsearchtest.request.DeleteCommentDtoRequest;
+import com.example.elasticsearchtest.request.ModifyCommentDtoRequest;
 import com.example.elasticsearchtest.response.ResponseDto;
 import com.example.elasticsearchtest.service.MongoDBService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.awt.print.Book;
 import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
 public class MongoTestController {
-    private final MongoRepository mongoRepository;
+    private final MongodbRepository mongodbRepository;
     private final MongoDBService mongoDBService;
-
     @PostMapping("/comment")
     public ResponseDto<?> create_comment(@RequestBody BookReviewRequest request) {
         //여기서는 받고 이제 service 부분에서 가보자..
         mongoDBService.create_comment(request);
         return ResponseDto.success("good");
+    }
+    @DeleteMapping("/delete_comment")
+    public ResponseDto<?> delete_comment(@RequestBody DeleteCommentDtoRequest request) {
+        //여기서는 받고 이제 service 부분에서 가보자..
+        mongoDBService.delete_comment(request.getDelete_id());
+        return ResponseDto.success("good");
 
     }
+    @PostMapping("/modify_comment")
+    public ResponseDto<?> modify_comment(@RequestBody ModifyCommentDtoRequest request) {
+        //여기서는 받고 이제 service 부분에서 가보자..
+        mongoDBService.modify_comment(request);
+        return ResponseDto.success("good");
+    }
+    @GetMapping("/get")
+    public ResponseDto<?> get_comment() {
+        //여기서는 받고 이제 service 부분에서 가보자..
+        List<BooksReview> all = mongodbRepository.findAll();
+        for (BooksReview booksReview : all) {
+            System.out.println("booksReview.getIsbn13() = " + booksReview.getIsbn13());
+            System.out.println("booksReview.getNickname() = " + booksReview.getNickname());
+            
+        }
+        return ResponseDto.success("good");
 
-    @DeleteMapping("/delete")
+    }
+    @DeleteMapping("/deleteAll")
     public ResponseEntity<String> delete() {
-        mongoRepository.deleteAll();
+        mongodbRepository.deleteAll();
         return ResponseEntity.ok("delete good");
     }
 
