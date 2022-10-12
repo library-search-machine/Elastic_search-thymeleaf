b7ca416193641bc5c0a9cc61d6faab797d309688
DongDongShin <tlsehdgk87@naver.com>
2022-10-13 03:09:06 +0900

    feat: 작성된 코멘트들중 해당 닉네임으로 작성된 코멘트들을 List로 리턴

--- src/main/java/com/example/elasticsearchtest/repository/MongodbRepository.java
+++ src/main/java/com/example/elasticsearchtest/repository/MongodbRepository.java
@@ -8,5 +8,6 @@ import java.util.List;
 public interface MongodbRepository extends MongoRepository<BooksReview,String> {
 
     List<BooksReview> findByIsbn13(String isbn13);
+    List<BooksReview> findByNickname(String nickname);
 
 }
